33ff49718e6d02fd105657f7db657f3f8628aa60
Jeffrey Walton <noloader@gmail.com>
2019-09-29 00:38:53 -0400

    Add X.509 certificate example

--- pem_create_keys.sh
+++ pem_create_keys.sh
@@ -145,6 +145,8 @@ EOF
 openssl req -config example-com.conf -new -x509 -sha256 -newkey rsa:2048 -nodes \
     -keyout example-com.key.pem -days 365 -out example-com.cert.pem
 
+# View cert with 'openssl x509 -in example-com.cert.pem -inform PEM -text -noout'
+
 ##################################
 # cacert.pem
 
